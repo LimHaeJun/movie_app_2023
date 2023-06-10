732360f36605aeb249c490aa49b52472177edfd6
LimHaeJun <6462jun@gmail.com>
2023-06-11 00:35:56 +0900

    04-4장 완료

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
-function Food({name,picture}) {
+function Food({name,picture,rating}) {
   return (
     <div>
       <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
       <img src={picture} alt={name} />
     </div>
   );
@@ -13,27 +15,32 @@ const foodILike = [
   { 
     id: 1,
     name: "kimchi",
-    image: 'https://t3.ftcdn.net/jpg/04/66/99/64/360_F_466996486_5u0HAf2FDdkv8RkJZc3rEv2gADlzyFhC.jpg'
+    image: 'https://t3.ftcdn.net/jpg/04/66/99/64/360_F_466996486_5u0HAf2FDdkv8RkJZc3rEv2gADlzyFhC.jpg',
+    rating: 5
   },
   {
     id: 2,
     name: "Samgiopsal",
-    image: 'https://post-phinf.pstatic.net/MjAyMDAzMDNfMTcg/MDAxNTgzMTkwNjA3ODQ5.kUXPHqGJ2xPDSu_3FiEoFC3kY9QyQ_g9CziCGrFSDuEg.LpCfOTbc5qth9d-GKzGv9jwj2VKhcqmPHp5cp1KJYEsg.JPEG/IM_food02.jpg?type=w1200'
+    image: 'https://post-phinf.pstatic.net/MjAyMDAzMDNfMTcg/MDAxNTgzMTkwNjA3ODQ5.kUXPHqGJ2xPDSu_3FiEoFC3kY9QyQ_g9CziCGrFSDuEg.LpCfOTbc5qth9d-GKzGv9jwj2VKhcqmPHp5cp1KJYEsg.JPEG/IM_food02.jpg?type=w1200',
+    rating: 4.3
   },
   {
     id: 3,
     name: "Bibimbap",
-    image: 'https://t4.ftcdn.net/jpg/05/14/61/81/360_F_514618189_sq7oLxZODqeOzH1yrHDL9kFepHqnB8Ft.jpg'
+    image: 'https://t4.ftcdn.net/jpg/05/14/61/81/360_F_514618189_sq7oLxZODqeOzH1yrHDL9kFepHqnB8Ft.jpg',
+    rating: 3.5
   },
   {
     id: 4,
     name: "Doncasu",
-    image: 'https://media.istockphoto.com/id/1134158942/ko/%EC%82%AC%EC%A7%84/%EB%8F%BC%EC%A7%80%EA%B3%A0%EA%B8%B0-%EB%8F%88%EA%B0%80%EC%8A%A4.jpg?s=612x612&w=0&k=20&c=C6v0QlT2p2gZfT9ytsYzAnMCKVOzYzWpKP6e1wkkGwY='
+    image: 'https://media.istockphoto.com/id/1134158942/ko/%EC%82%AC%EC%A7%84/%EB%8F%BC%EC%A7%80%EA%B3%A0%EA%B8%B0-%EB%8F%88%EA%B0%80%EC%8A%A4.jpg?s=612x612&w=0&k=20&c=C6v0QlT2p2gZfT9ytsYzAnMCKVOzYzWpKP6e1wkkGwY=',
+    rating: 4.8
   },
   {
     id:5,
     name: "Kimpap",
-    image: 'https://t3.ftcdn.net/jpg/04/22/49/54/360_F_422495424_AkP6hAHiYBhNxZ3kZUBuYouedhej37a3.jpg'
+    image: 'https://t3.ftcdn.net/jpg/04/22/49/54/360_F_422495424_AkP6hAHiYBhNxZ3kZUBuYouedhej37a3.jpg',
+    rating: 4
   },
 ];
 
@@ -41,10 +48,16 @@ function App() {
   return( 
     <div>
       {foodILike.map(dish => (
-        <Food key={dish.id} name ={dish.name} picture={dish.image} />
+        <Food key={dish.id} name ={dish.name} picture={dish.image} rating={dish.rating} />
       ))}
     </div>
   );
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
 
 export default App;
